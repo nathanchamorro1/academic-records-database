9ff39e27c32562908d085d50349aca792c453615
nathanchamorro1 <160960078+nathanchamorro1@users.noreply.github.com>
2025-05-04 17:52:29 -0700

    created two tables so far

---
+++ .history/tables_20250504174902.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Professor (
+    SSN int PRIMARY KEY;
+    FirstName varchar(255);
+    LastName varchar(255);
+    TelephoneNumber int(10)
+    Sex varchar(1);
+    Title varchar(255);
+    Salary varchar(255);
+    CollegeDegrees varchar(255);
+
+)
+
+CREATE TABLE Department (
+    DepartmentID int PRIMARY KEY;
+    DeptName varchar(255);
+    Telephone int(10)
+    
+
+
+)
